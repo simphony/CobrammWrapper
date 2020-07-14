9c13464d5064e63bb10e0ca7d60c3da1d39ae2eb
Matteo Bonfanti <mat.bonfanti@gmail.com>
2020-07-14 15:00:21 +0200

    Added gitignore and setup files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+from packageinfo import VERSION, NAME, OSP_CORE_MIN, OSP_CORE_MAX
+
+# Read description
+with open('README.md', 'r') as readme:
+    README_TEXT = readme.read()
+
+
+# main setup configuration class
+setup(
+    name=NAME,
+    version=VERSION,
+    author='Computational Chemistry CHIMIND, UNIBO',
+    description='Simulation wrapper for COBRAMM',
+    keywords='simphony, cuds, cobramm',
+    long_description=README_TEXT,
+    install_requires=[
+        'osp-core>=' + OSP_CORE_MIN + ', <' + OSP_CORE_MAX,
+    ],
+    packages=find_packages(),
+    test_suite='tests',
+    entry_points={
+        'wrappers':
+            'wrapper = osp.wrappers.cobrammwrapper:CobrammSession'},
+)
