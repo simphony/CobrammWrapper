2597e38ac9609684eab58a9e4f7eb39dd517435a
Matteo Bonfanti <matteo.bonfanti@unibo.it>
2020-08-27 17:54:08 +0200

    update of cobramm_session.py following the new version of the ontology

--- osp/wrappers/cobrammwrapper/cobramm_session.py
+++ osp/wrappers/cobrammwrapper/cobramm_session.py
@@ -31,20 +31,31 @@ class CobrammSession(SimWrapperSession):
                 if obj.is_a(cobramm.position):
                     atom = obj.get(rel=cobramm.is_part_of)[0]
                     pos = self._engine.get_position(atom.uid)
-                if pos is not None:
-                    obj.value = pos
+                    if pos is not None:
+                        obj.vector_value = pos
 
-            yield obj
-        else:
-            yield None
+                yield obj
+            else:
+                yield None
 
     # OVERRIDE
     def _apply_added(self, root_obj, buffer):
         """Adds the added cuds to the engine."""
         for obj in buffer.values():
+            # add a new atom
             if obj.is_a(cobramm.atom):
-                pos = obj.get(oclass=cobramm.position)[0].value
-                self._engine.add_atom(obj.uid, pos)
+                pos = obj.get(oclass=cobramm.position)[0].vector_value
+                sym = obj.get(oclass=cobramm.element_id)[0].atom_symbol
+                self._engine.add_atom(obj.uid, sym, pos)
+            # specify temperature of the simulation
+            elif obj.is_a(cobramm.temperature):
+                self._engine.add_property("temperature", obj.scalar_value)
+            # specify pressure of the simulation
+            elif obj.is_a(cobramm.pressure):
+                self._engine.add_property("pressure", obj.scalar_value)
+            # give solvent commercial name
+            elif obj.is_a(cobramm.solvent):
+                self._engine.add_property("solvent", obj.commercial_name)
 
     # OVERRIDE
     def _apply_updated(self, root_obj, buffer):
@@ -53,14 +64,35 @@ class CobrammSession(SimWrapperSession):
             # check if position has been updated
             if obj.is_a(cobramm.position):
                 atom = obj.get(rel=cobramm.is_part_of)[0]
-                self._engine.update_position(atom.uid, obj.value)
+                sym = atom.get(oclass=cobramm.element_id)[0].atom_symbol
+                self._engine.add_atom(atom.uid, sym, obj.vector_value)
+            # check if atomic symbol has been updated
+            elif obj.is_a(cobramm.element_id):
+                atom = obj.get(rel=cobramm.is_part_of)[0]
+                pos = atom.get(oclass=cobramm.position)[0].vector_value
+                self._engine.add_atom(atom.uid, obj.atom_symbol, pos)
+            # check if temperature has changed
+            elif obj.is_a(cobramm.temperature):
+                self._engine.add_property("temperature", obj.scalar_value)
+            # check if pressure has changed
+            elif obj.is_a(cobramm.pressure):
+                self._engine.add_property("pressure", obj.scalar_value)
+            # check if solvent has changed
+            elif obj.is_a(cobramm.solvent):
+                self._engine.add_property("solvent", obj.commercial_name)
 
     # OVERRIDE
     def _apply_deleted(self, root_obj, buffer):
         """Deletes the deleted cuds from the engine."""
-        for cuds_object in buffer.values():
-            if cuds_object.is_a(cobramm.atom):
-                self._engine.delete_atom(cuds_object.uid)
+        for obj in buffer.values():
+            if obj.is_a(cobramm.atom):
+                self._engine.delete_atom(obj.uid)
+            elif obj.is_a(cobramm.temperature):
+                self._engine.delete_property("temperature")
+            elif obj.is_a(cobramm.pressure):
+                self._engine.delete_property("pressure")
+            elif obj.is_a(cobramm.solvent):
+                self._engine.delete_property("solvent")
 
     # OVERRIDE
     def _initialise(self, root_obj):
