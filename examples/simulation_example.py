efdeca0103fdd3ee97e987216cbb9554da343338
Alessandro Calvio <alessandro.calvio2@unibo.it>
2022-10-26 23:45:52 +0200

    test - update simulation_example.py with latest changes

--- examples/simulation_example.py
+++ examples/simulation_example.py
@@ -3,7 +3,6 @@ from osp.core import cobramm
 from osp.wrappers.cobrammwrapper import CobrammSession
 from osp.core.utils import pretty_print
 
-material = cobramm.material()
 
 # the following is the geometry of a formaldehyde molecule
 atoms_to_add = [["C", [0.60720,  0.00000, -0.00040]],
@@ -11,32 +10,55 @@ atoms_to_add = [["C", [0.60720,  0.00000, -0.00040]],
                 ["H", [1.14720,  0.93530,  0.00160]],
                 ["H", [1.14720, -0.93530,  0.00160]]]
 
-# define the material by specifying atom symbols and cartesian coordinates, atom-by-atom
-for atom_symbol, atom_coords in atoms_to_add:
-    atom = material.add(cobramm.atom())
-    atom.add(cobramm.position(vector_value=atom_coords, unit="Ang"))
-    atom.add(cobramm.element_id(atom_symbol=atom_symbol))
-
-# add other physical properties: temperature, pressure, solvent (with commercial name)
-material.add(cobramm.temperature(scalar_value=300., unit="K"))
-material.add(cobramm.pressure(scalar_value=1., unit="bar"))
-material.add(cobramm.solvent(commercial_name="water"))
-
-# pretty-print the material so far
-print("\n")
-pretty_print(material)
-print("\n")
 
 # now open a new CobrammSession to execute the simulation
 with CobrammSession() as session:
     wrapper = cobramm.wrapper(session=session)
-    material_wrapper = wrapper.add(material, rel=cobramm.has_part)
+
+    # Create input ontological object
+    system = cobramm.system()
+    material = cobramm.material()
+
+    # define the material by specifying atom symbols and cartesian coordinates, atom-by-atom
+    for atom_symbol, atom_coords in atoms_to_add:
+        atom = material.add(cobramm.atom())
+        atom.add(cobramm.position(vector_value=atom_coords, unit="Ang"))
+        atom.add(cobramm.element_id(atom_symbol=atom_symbol))
+
+    material.add(cobramm.charge(integer_value=0))
+    system.add(material)
+
+    molecule = cobramm.molecule(commercial_name="water")
+    solvent = cobramm.solvent()
+    solvent.add(molecule)
+    system.add(solvent)
+
+    temperature = cobramm.temperature(scalar_value=300, unit="K")
+    system.add(temperature)
+
+    pressure = cobramm.pressure(scalar_value=1, unit="bar")
+    system.add(pressure)
+
+    # Set accuracy
+    accuracy = cobramm.accuracy(accuracy_value=1)
+
+    # Set case
+    case = cobramm.case(case_name="absorption")
+
+    system_wrapper = wrapper.add(system, rel=cobramm.has_part)
+    accuracy_wrapper = wrapper.add(accuracy, rel=cobramm.has_part)
+    case_wrapper = wrapper.add(case, rel=cobramm.has_part)
+
+    # pretty-print the material so far
+    print("\n")
+    pretty_print(system)
+    print("\n")
 
     # run the simulation
-    material_wrapper.session.run()
+    wrapper.session.run()
 
     # extract the spectrum
-    spectrum = material_wrapper.get(oclass=cobramm.spectrum)[0]
+    spectrum = (wrapper.get(oclass=cobramm.system)[0]).get(oclass=cobramm.spectrum)[0]
 
     # pretty-print the final spectrum
     print("\n")
